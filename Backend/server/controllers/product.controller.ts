64d855ef44e595c477cdc6708f799d2cc9b4aa19
Jelle <jelle.demets@student.howest.be>
2022-01-11 17:55:25 +0100

    brand requests

--- Backend/server/controllers/product.controller.ts
+++ Backend/server/controllers/product.controller.ts
@@ -18,5 +18,29 @@ import { CrudController, IController, ICrudController } from './crud.controller'
       super(Product) // Initialize the parent constructor
       this.router.get('/all', this.all)
       this.router.get('/:id', this.one)
+      this.router.get('/productsFromCategory/:brandId/:categoryId', this.getProductsFromCategoryAndBrand)
+    }
+
+    getProductsFromCategoryAndBrand = async (request: Request, response: Response, next: NextFunction) => {
+      try{
+        const brandId = request.params.brandId
+        const categoryId = request.params.categoryId
+        
+        if(brandId === undefined || categoryId === undefined) 
+          return response.status(500).json({error :  "Missing parameter" })
+        
+        console.log(brandId, categoryId)
+        const data = await this.repository
+        .createQueryBuilder('p')
+        .select(['p.Name','p.Picture','p.Price','b.Name','b.BrandId','c.CategoryId','c.Name'])
+        .innerJoin('p.Brand','b')
+        .innerJoin('p.Category','c')
+        .where('b.BrandId = :id', { id: brandId })
+        //.andWhere('c.CategoryId = :id', { id: categoryId})
+        .getMany()
+        response.send(data)
+      } catch(error){
+        response.status(500).json({error : { error }})
+      }
     }
 }
