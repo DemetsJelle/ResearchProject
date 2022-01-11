07f1b9eadf39983decc91e6fc30c172e3c89baa5
Jelle <jelle.demets@student.howest.be>
2022-01-11 20:14:47 +0100

    update wishlist

--- Backend/server/controllers/wishlist.controller.ts
+++ Backend/server/controllers/wishlist.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction, Router, request } from 'express'
+import { Product } from '../entities/product'
 import { Wishlist} from '../entities/wishlist'
 import { CrudController, IController, ICrudController } from './crud.controller'
 
@@ -25,7 +26,7 @@ import { CrudController, IController, ICrudController } from './crud.controller'
       try {
         let result:any
         if (request.body.data === null) 
-          response.status(406).json({error: 'No data has been provided'})
+          return response.status(406).json({error: 'No data has been provided'})
         
         const newWishlist:Wishlist = {
           //WishlistId: "35a6e9b7-6714-4147-82bf-679552ec0dc1",
@@ -40,6 +41,43 @@ import { CrudController, IController, ICrudController } from './crud.controller'
         result = await this.repository.save(addWishlist)
 
         if(result.WishlistId) 
+          return response.status(200).json({succes: true})
+
+        else 
+          return response.status(500).json({error: "Something went wrong"})
+        
+      } catch (error) {
+        response.status(500).json({error:{error}})
+      }
+    }
+
+    updateWishlist = async (request: Request, response: Response, next: NextFunction) => {
+      try {
+        if(request.params.id === undefined)
+          return response.status(406).json({error: 'No data has been provided'})
+
+        if (request.body.data === null) 
+          return response.status(406).json({error: 'No data has been provided'})
+
+        let wishlistId = request.params.id
+        let result:any
+
+        const oldProducts:Wishlist = await this.repository.findOne(wishlistId)
+        let newData:Product[]
+        
+       oldProducts.Product.forEach((p) => {
+         newData.push(p)
+       })
+       newData.push(request.body.product)
+        
+        const update = await this.repository
+          .createQueryBuilder()
+          .update(Wishlist)
+          .set({ Product: newData})
+          .where('WishlistId = :id',{ id: wishlistId })
+          .execute()
+
+        if(update.affected === 1)
           return response.status(200).json({succes: true})
 
         else 
