50ea05dc87a96cf9c355e481cb4b86112bf023af
Jelle <jelle.demets@student.howest.be>
2022-01-16 11:44:11 +0100

    added go back buttons

--- Webshop/Frontend/src/routes/productDetail.svelte
+++ Webshop/Frontend/src/routes/productDetail.svelte
@@ -5,6 +5,7 @@
     import { get, post } from '../utils/useApi'
 
     import authStore from '../stores/authStore'
+import { goto } from '$app/navigation';
 
     let alt:string = 'Picture of the product'
     let pictureUrl:string
@@ -30,9 +31,36 @@
           console.log('succesfully saved')
         }
     }
+
+    const goBack = () => {
+        goto('/')
+    }
 </script>
 
 {#if productInfo}
+    <section
+        class="pb-4 flex hover:cursor-pointer hover:font-bold"
+        on:click={goBack}
+    >
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 my-auto"
+        viewBox="0 0 20.828 37.657"
+        >
+            <path
+                id="chevron-down"
+                d="M6,9,22,25,38,9"
+                transform="translate(27 -3.172) rotate(90)"
+                fill="none"
+                stroke="#686868"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="4"
+            />
+        </svg>
+
+        <p class="">Go back</p>
+    </section>
     <section class="m-4">
         <div class="flex justify-between">
             <h1>{productInfo.Name}</h1>
