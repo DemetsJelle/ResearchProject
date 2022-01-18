11862b1e0a95b9232eda71fc3cead2ac3432dd95
Jelle <jelle.demets@student.howest.be>
2022-01-18 14:18:53 +0100

    testing

---
+++ Webshop/Frontend/src/routes/test.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Client, Segment } from "@speechly/browser-client";
+ 
+    let microphone = false
+    let speech
+ 
+    const client = new Client({
+        appId:'someId',
+        language: 'en-US'
+    });
+    client.initialize();
+ 
+    const speak = () => {
+         microphone = !microphone
+ 
+         if(microphone)
+             client.startContext();
+         else
+             client.stopContext();
+    }
+ 
+    client.onSegmentChange((segment) => {
+         console.log('Received new segment from the API:', segment.intent, segment.entities, segment.words, segment.isFinal)
+     })
+ </script>
+ 
+ <section>
+     <button
+         on:click = {speak}
+     >
+         MICROPHONE
+     </button>
+ </section>
