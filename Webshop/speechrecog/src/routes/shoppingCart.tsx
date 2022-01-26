ec418afb57f782170c091bbeff94cf8708a52ba0
Jelle <jelle.demets@student.howest.be>
2022-01-26 21:24:34 +0100

    fixed design + functionaliteiten wishlist page

--- Webshop/speechrecog/src/routes/shoppingCart.tsx
+++ Webshop/speechrecog/src/routes/shoppingCart.tsx
@@ -3,7 +3,7 @@ import React, {ChangeEvent, useEffect, useContext, useState, createContext} from
 import { atom, useRecoilState} from 'recoil'
 import '../style/shoppingCart.css'
 import ShoppingListItem from '../components/shoppingListItemComponent';
-
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 export const totalPriceState = atom({
     key:"totalPrice",
@@ -11,6 +11,9 @@ export const totalPriceState = atom({
 })
 
 function ShoppingCart(){
+    const [showTranscript, setshowTranscript] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [micState, setMicState] = useState<boolean>(false)
     const [shoppingCart, setShoppingCart] = useState<any[]>()
     const [totalPrice, setTotalPrice] = useRecoilState(totalPriceState)
 
@@ -28,6 +31,37 @@ function ShoppingCart(){
     const goBack = () => {
         window.location.href=`/`
     }
+
+    const commands = [
+        {
+            command: ['terug'],
+            callback: () => goBack(),
+            doel:'filter',
+            voorbeeld:["zoek 'boxy ski jas'","search 'boxy ski jas'"],
+            uitleg: "Zoeken naar boxy ski jas"
+        },
+    ]
+
+    const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition, interimTranscript } = useSpeechRecognition({ commands });
+
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+      }
+    
+      const startListening = () => {
+        //setIsLoading(true)
+        setMicState(!micState)
+        setshowTranscript(!showTranscript)
+    
+        if(micState === true)
+          SpeechRecognition.stopListening();
+          
+        else if(micState === false)
+          SpeechRecognition.startListening({continuous: true});
+          setIsLoading(true);
+    
+        resetTranscript()
+      }
 
     return (
         <main className='main'>
@@ -74,6 +108,51 @@ function ShoppingCart(){
                     <button className="checkout_btn">Verder naar bestellen</button>
                 </div>
             </div>
+
+            <div className="voiceControl">
+        {showTranscript && 
+          <div className="showTranscript">
+            <p className="showTranscript_text">Zeg iets als:</p>
+            {/* <div className="showTranscript_example_container">
+              <h2 className="showTranscript_example">'Zoek jassen van Protest'</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" className="showTranscript_icon" viewBox="0 0 33 33">
+                <g id="Group_5" data-name="Group 5" transform="translate(343.828 489.635)">
+                  <path id="Icon_awesome-microphone" data-name="Icon awesome-microphone" d="M6,12A3.273,3.273,0,0,0,9.273,8.727V3.273a3.273,3.273,0,0,0-6.545,0V8.727A3.273,3.273,0,0,0,6,12Zm5.455-5.455h-.545a.545.545,0,0,0-.545.545V8.727a4.369,4.369,0,0,1-4.8,4.342A4.5,4.5,0,0,1,1.636,8.533V7.091a.545.545,0,0,0-.545-.545H.545A.545.545,0,0,0,0,7.091V8.46a6.188,6.188,0,0,0,5.182,6.194v1.164H3.273a.545.545,0,0,0-.545.545v.545a.545.545,0,0,0,.545.545H8.727a.545.545,0,0,0,.545-.545v-.545a.545.545,0,0,0-.545-.545H6.818V14.667A6.006,6.006,0,0,0,12,8.727V7.091A.545.545,0,0,0,11.455,6.545Z" transform="translate(-333.328 -477.998)"/>
+                  <path id="Path_4" data-name="Path 4" d="M33,18A15,15,0,1,1,18,3,15,15,0,0,1,33,18Z" transform="translate(-345.328 -491.135)" fill="none" stroke="#000" sstrokeLinecap="round" trokeLinejoin="round" strokeWidth="3"/>
+                  <ellipse id="Ellipse_1" data-name="Ellipse 1" cx="3" cy="2.875" rx="3" ry="2.875" transform="translate(-330.328 -484.373)"/>
+                </g>
+              </svg>
+            </div> */}
+            <p className="showTranscript_example">'Zoek jassen van Protest'</p>
+            <h3 className="showTranscript_transcript">{transcript}</h3>
+            {isLoading &&
+              <div className="ballsContainer">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            }
+            <button className="showTranscript_all_commandos">alle commando's</button>
+          </div>
+        }
+        <button className="speechButton"
+          onClick={startListening}
+        >
+          {
+            micState ?  
+              <div className="boxContainer">
+                <div className="box box1"></div>
+                <div className="box box2"></div>
+                <div className="box box3"></div>
+                <div className="box box4"></div>
+                <div className="box box5"></div>
+            </div> :
+            <svg xmlns="http://www.w3.org/2000/svg" className="speechButton_icon" viewBox="0 0 24.75 36"fill="#368ADE" >
+              <path id="Icon_awesome-microphone" data-name="Icon awesome-microphone" d="M12.375,24.75A6.75,6.75,0,0,0,19.125,18V6.75a6.75,6.75,0,0,0-13.5,0V18A6.75,6.75,0,0,0,12.375,24.75ZM23.625,13.5H22.5a1.125,1.125,0,0,0-1.125,1.125V18a9.01,9.01,0,0,1-9.9,8.956,9.273,9.273,0,0,1-8.1-9.357V14.625A1.125,1.125,0,0,0,2.25,13.5H1.125A1.125,1.125,0,0,0,0,14.625v2.824A12.762,12.762,0,0,0,10.688,30.224v2.4H6.75A1.125,1.125,0,0,0,5.625,33.75v1.125A1.125,1.125,0,0,0,6.75,36H18a1.125,1.125,0,0,0,1.125-1.125V33.75A1.125,1.125,0,0,0,18,32.625H14.063V30.251A12.387,12.387,0,0,0,24.75,18V14.625A1.125,1.125,0,0,0,23.625,13.5Z"/>
+            </svg>
+          }
+        </button>
+      </div>
         </main>
     )
 }
--- Webshop/speechrecog/src/routes/wishlist.tsx
+++ Webshop/speechrecog/src/routes/wishlist.tsx
@@ -1,12 +1,106 @@
 import React, {ChangeEvent, useEffect, useState, MouseEvent} from 'react';
+import '../style/wishlist.css'
 
 function Wishlist(){
+    const [wishlist, setWishlist] = useState<any[]>()
 
+    useEffect(() =>{
+        const list:any = localStorage.getItem('wishlist')
+        const parsedList = JSON.parse(list)
+        setWishlist(parsedList)
+        console.log(parsedList)
+    },[])
+
+    const goBack = () => {
+        window.location.href=`/`
+    }
+
+    const localSWishlist:any[] = []
+    const removeItemFromWishlist = (productId:any) => {
+        const list:any = localStorage.getItem('wishlist')
+        if(list){
+            const parsedList = JSON.parse(list)
+            parsedList.forEach((item:any) => {
+                if(item.ProductId !== productId)
+                    localSWishlist.push(item)
+            })
+        }
+        setWishlist(localSWishlist)
+        localStorage.setItem('wishlist', JSON.stringify(localSWishlist))
+    }
+
+    const localSShoppingCart:any[] = []
+    const addToShoppingList = (newItem:any) =>{
+        const list:any = localStorage.getItem('shoppingCart')
+        if(list){
+            const parsedList = JSON.parse(list)
+            parsedList.forEach((item:any) => {
+                localSShoppingCart.push(item)
+            })
+        }
+        localSShoppingCart.push(newItem)
+        localStorage.setItem('shoppingCart', JSON.stringify(localSShoppingCart))
+    }
+
+    const navigateToDetail = (productId:any) => {
+        window.location.href=`/detailsProduct/${productId}`
+    }
 
     return (
-        <div>
-            Wishlist
-        </div>
+        <main className="main">
+            <div>
+                <div className="goBack_container" onClick={goBack}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="goBack_icon" viewBox="0 0 24.743 23.34">
+                        <path id="Icon_ionic-md-arrow-round-back" data-name="Icon ionic-md-arrow-round-back" d="M28.223,15.75H13.177l5.836-5.583a2.326,2.326,0,0,0,0-3.178,2.079,2.079,0,0,0-3.038,0L6.258,16.411a2.142,2.142,0,0,0-.633,1.575v.028a2.142,2.142,0,0,0,.633,1.575l9.71,9.422a2.079,2.079,0,0,0,3.037,0,2.326,2.326,0,0,0,0-3.178L13.17,20.25H28.216A2.2,2.2,0,0,0,30.368,18,2.178,2.178,0,0,0,28.223,15.75Z" transform="translate(-5.625 -6.33)" fill="#368ade"/>
+                    </svg>
+                    <p>vorige</p>
+                </div>
+                <div className="goBack_container goBack_command">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="productSection_afkorting_icon" viewBox="0 0 24.75 36">
+                        <path id="Icon_awesome-microphone" data-name="Icon awesome-microphone" d="M12.375,24.75A6.75,6.75,0,0,0,19.125,18V6.75a6.75,6.75,0,0,0-13.5,0V18A6.75,6.75,0,0,0,12.375,24.75ZM23.625,13.5H22.5a1.125,1.125,0,0,0-1.125,1.125V18a9.01,9.01,0,0,1-9.9,8.956,9.273,9.273,0,0,1-8.1-9.357V14.625A1.125,1.125,0,0,0,2.25,13.5H1.125A1.125,1.125,0,0,0,0,14.625v2.824A12.762,12.762,0,0,0,10.688,30.224v2.4H6.75A1.125,1.125,0,0,0,5.625,33.75v1.125A1.125,1.125,0,0,0,6.75,36H18a1.125,1.125,0,0,0,1.125-1.125V33.75A1.125,1.125,0,0,0,18,32.625H14.063V30.251A12.387,12.387,0,0,0,24.75,18V14.625A1.125,1.125,0,0,0,23.625,13.5Z"/>
+                    </svg>
+                    <p>terug</p>
+                </div>
+            </div>
+
+            <div className="wishlist_grid">
+                {wishlist && wishlist.map(item => {
+                    return(
+                        <div className="wishlist_item_container">
+                            <img
+                                onClick={() => navigateToDetail(item.ProductId)}
+                                className="wishlist_item_img"
+                                src = {item.Picture}
+                                alt =  {`Image of ${item.Name}`}
+                            />
+
+                            <div className="wishlist_item_buttonC">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="cart_icon" viewBox="0 0 30 30">
+                                    <path id="Icon_material-shopping-cart" data-name="Icon material-shopping-cart" d="M10.5,27a3,3,0,1,0,3,3A3,3,0,0,0,10.5,27ZM1.5,3V6h3L9.9,17.385,7.875,21.06A2.9,2.9,0,0,0,7.5,22.5a3.009,3.009,0,0,0,3,3h18v-3H11.13a.371.371,0,0,1-.375-.375l.045-.18L12.15,19.5H23.325a2.986,2.986,0,0,0,2.625-1.545L31.32,8.22a1.465,1.465,0,0,0,.18-.72A1.5,1.5,0,0,0,30,6H7.815L6.405,3Zm24,24a3,3,0,1,0,3,3A3,3,0,0,0,25.5,27Z" transform="translate(-1.5 -3)" fill="#368ade"/>
+                                </svg>
+                                <button onClick={() => addToShoppingList(item)} className="wishlist_item_button_cart">Winkelmand</button>
+                            </div>
+
+                            <div 
+                                className="wishlist_item_text"
+                                onClick={() => navigateToDetail(item.ProductId)}    
+                            >
+                                <h1 className="wishlist_item_name">{item.Name}</h1>
+                                <div className="wishlist_item_priceC">
+                                    <p className="wishlist_item_price">€{item.Price}</p>
+                                    <div className="wishlist_item_removebtn">
+                                        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => removeItemFromWishlist(item.ProductId)} className="wishlist_item_icon" viewBox="0 0 21 27">
+                                            <path id="Icon_material-delete" data-name="Icon material-delete" d="M9,28.5a3.009,3.009,0,0,0,3,3H24a3.009,3.009,0,0,0,3-3v-18H9ZM28.5,6H23.25l-1.5-1.5h-7.5L12.75,6H7.5V9h21Z" transform="translate(-7.5 -4.5)"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    )
+                })}
+            </div>
+        </main>
     )
 }
 
