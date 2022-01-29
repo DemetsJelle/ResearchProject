f9b1f320ed86e1b6027450ce0db43d6beebcaceb
Jelle <jelle.demets@student.howest.be>
2022-01-29 16:15:16 +0100

    responsive tablet

--- Webshop/speechrecog/src/App.tsx
+++ Webshop/speechrecog/src/App.tsx
@@ -486,7 +486,6 @@ function App() {
               <svg xmlns="http://www.w3.org/2000/svg" onClick={openInfo} className='showInfo_icon' viewBox="0 0 21 21">
                 <path id="Icon_material-close" data-name="Icon material-close" d="M28.5,9.615,26.385,7.5,18,15.885,9.615,7.5,7.5,9.615,15.885,18,7.5,26.385,9.615,28.5,18,20.115,26.385,28.5,28.5,26.385,20.115,18Z" transform="translate(-7.5 -7.5)"/>
               </svg>
-
             </div>
             <h2 className="showInfo_item_title">Producten filteren</h2>
             <div className="showInfo_item_command">
@@ -496,7 +495,6 @@ function App() {
                 </svg>
                 <p>Zoek "boxy ski jas"</p>
               </div>
-                
              </div>
              <div className="showInfo_item_command">
               <div className="test">
